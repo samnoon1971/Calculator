eca58c837ac90722fe4e0685cfd844decbb446ea
Samnoon Abrar <samnoonabrar@gmail.com>
2021-04-12 11:04:54 +0600

    fixup! Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Display from "./component/Display";
+import calculate from "./logic/calculate";
+import ButtonPanel from "./component/ButtonPanel"
+import React from "react";
+
+export default class App extends React.Component{
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  }
+  render(){
+    return(
+        <div className="component-app">
+          <Display value={this.state.next || this.state.total || "0"} />
+          <ButtonPanel clickHandler={this.handleClick} />
+        </div>
+    );
+  }
+}
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,22 +1,24 @@
 import Big from "big.js";
 export default function operate(numberOne, numberTwo, operation){
     const one = Big(numberOne || "0");
-    const two = Big(NumberTwo || (operation === "÷" || operation === "x" ? "1" : "0"));
+    const two = Big(numberTwo || (operation === "÷" || operation === "x" ? "1" : "0"));
     if(operation === "-"){
-        return one.plus(two).toString();
-    }
-    else if(operation === '+'){
         return one.minus(two).toString();
     }
-    else if(operation === '*'){
+    else if(operation === '+'){
+        return one.plus(two).toString();
+    }
+    else if(operation === 'x'){
         return one.times(two).toString();
     }
     else if(operation === "÷"){
-       if(two == "0"){
+       if(two === "0"){
            alert("Can not divide by zero.");
            return "0";
        }
-       return one.div(two).toString();
+       else {
+           return one.div(two).toString();
+       }
     }
-    throw Error(`Unknown Operation ${operation}can not be performed`);
+    throw Error(`Unknown Operation '${operation}'`);
 }
